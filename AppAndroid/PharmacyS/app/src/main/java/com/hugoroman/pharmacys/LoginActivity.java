4e666944af549198f684cdbd74d31e3b11faad7d
hugomc92 <hugomc92@gmail.comDarks>
2016-04-26 15:34:19 +0200

    App Android

---
+++ AppAndroid/PharmacyS/app/src/main/java/com/hugoroman/pharmacys/LoginActivity.java
@@ -0,0 +1,154 @@
+package com.hugoroman.pharmacys;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+    private static final int REQUEST_SIGNUP = 0;
+
+    private EditText iEmail;
+    private EditText iPass;
+    private Button btnLogin;
+    private TextView linkSignUp;
+
+    private Toast exitToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        iEmail = (EditText) findViewById(R.id.input_email);
+        iPass = (EditText) findViewById(R.id.input_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        linkSignUp = (TextView) findViewById(R.id.link_signup);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Loguearse
+                login();
+            }
+        });
+
+        linkSignUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Lanzar la actividad de registrarse
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+
+    }
+
+    private void login() {
+
+        if(!validate()) {
+            onLoginFailed();
+
+            return;
+        }
+
+        btnLogin.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppThemeDialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = iEmail.getText().toString();
+        String password = iPass.getText().toString();
+
+        // Lógica del login
+        new android.os.Handler().postDelayed(
+            new Runnable() {
+
+                @Override
+                public void run() {
+                    // Cuando se complete, se decide si llamar a uno u a otro en función de la respuesta del servidor.
+                    // Si es correcto se tiene que guardar en el SharedPreferences
+                    onLoginSuccess();
+                    //onLoginFailed();
+                    progressDialog.dismiss();
+                }
+            }, 3000);
+    }
+
+    public void onLoginSuccess() {
+
+        setResult(RESULT_OK, null);
+
+        finish();
+    }
+
+    public void onLoginFailed() {
+
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        btnLogin.setEnabled(true);
+    }
+
+    public boolean validate() {
+
+        boolean valid = true;
+
+        String email = iEmail.getText().toString();
+        String password = iPass.getText().toString();
+
+        if(email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            iEmail.setError("enter a valid email address");
+            valid = false;
+        }
+        else {
+            iEmail.setError(null);
+        }
+
+        if(password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            iPass.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        }
+        else {
+            iPass.setError(null);
+        }
+
+        return valid;
+    }
+
+    // Capturar el evento cuando se registre el usuario si así lo ha pulsado
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // Terminamos la actividad de login y volvemos a la main
+                onLoginSuccess();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(exitToast!=null && exitToast.getView().getWindowToken() != null) {
+            finishAffinity();
+        }
+        else {
+            exitToast = Toast.makeText(this, R.string.exit, Toast.LENGTH_SHORT);
+            exitToast.show();
+        }
+    }
+}
---
+++ AppAndroid/PharmacyS/app/src/main/java/com/hugoroman/pharmacys/SignUpActivity.java
@@ -0,0 +1,136 @@
+package com.hugoroman.pharmacys;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SignUpActivity extends Activity {
+
+    private EditText iName;
+    private EditText iEmail;
+    private EditText iPass;
+    private Button btnSignUp;
+    private TextView linkLogin;
+
+    private Toast exitToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        iName = (EditText) findViewById(R.id.input_name);
+        iEmail = (EditText) findViewById(R.id.input_email);
+        iPass = (EditText) findViewById(R.id.input_password);
+        btnSignUp = (Button) findViewById(R.id.btn_login);
+        linkLogin = (TextView) findViewById(R.id.link_signup);
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Registrarse
+                signup();
+            }
+        });
+
+        linkLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Terminar la actividad para volver a la anterior
+                finish();
+            }
+        });
+
+    }
+
+    private void signup() {
+
+        if(!validate()) {
+            onSignUpFailed();
+
+            return;
+        }
+
+        btnSignUp.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this, R.style.AppThemeDialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Creating account...");
+        progressDialog.show();
+
+        String email = iEmail.getText().toString();
+        String password = iPass.getText().toString();
+
+        // Lógica del registro
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // Cuando se complete, se decide si llamar a uno u a otro en función de la respuesta del servidor.
+                        // Si es correcto se tiene que guardar en el SharedPreferences
+                        onSignUpSuccess();
+                        //onSignUpFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+    public void onSignUpSuccess() {
+
+        setResult(RESULT_OK, null);
+
+        finish();
+    }
+
+    public void onSignUpFailed() {
+
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        btnSignUp.setEnabled(true);
+    }
+
+    public boolean validate() {
+
+        boolean valid = true;
+
+        String name = iName.getText().toString();
+        String email = iEmail.getText().toString();
+        String password = iPass.getText().toString();
+
+        if(name.isEmpty() || name.length() < 3) {
+            iName.setError("at least 3 characters");
+            valid = false;
+        }
+        else {
+            iName.setError(null);
+        }
+
+        if(email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            iEmail.setError("enter a valid email address");
+            valid = false;
+        }
+        else {
+            iEmail.setError(null);
+        }
+
+        if(password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            iPass.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        }
+        else {
+            iPass.setError(null);
+        }
+
+        return valid;
+    }
+}
